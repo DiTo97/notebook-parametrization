89268115c140402e71bd8c02752011fad457920c
Federico Minutoli <40361744+DiTo97@users.noreply.github.com>
2024-02-05 17:44:00 +0100

    update params in run_notebook.sh

--- run_notebook.sh
+++ run_notebook.sh
@@ -2,23 +2,54 @@
 #
 # run_notebook.sh - It runs notebook parametrization in the background
 # --------------------------------------------------------------------
-# TODO: It should verify the $@ args
-# TODO: It should add a usage message
+configdir="configs"
+logdir="logs"
+background=0
+
+if [ $# -lt 2 ]; then
+    echo "Error: not enough arguments"
+    echo "Usage: $0 notebook config [--configdir <configdir>] [--logdir <logdir>] [--background]"
+    exit 1
+fi
+
 notebook=$1
 config=$2
 
-parameters="configs/$config.yaml"
+shift 2
 
-stdout="logs/$config.out"
-stderr="logs/$config.err"
+while (( "$#" )); do
+    case "$1" in
+        --configdir)
+          configdir=$2
+          shift 2
+          ;;
+        --logdir)
+          logdir=$2
+          shift 2
+          ;;
+        --background)
+          background=1
+          shift
+          ;;
+        *)
+          echo "Error: invalid argument"
+          echo "Usage: $0 notebook config [--configdir <configdir>] [--logdir <logdir>] [--background]"
+          exit 1
+    esac
+done
+
+parameters="$configdir/$config.yaml"
+
+stdout="$logdir/$config.out"
+stderr="$logdir/$config.err"
 
 python preprocessing.py $notebook
 
 run_notebook="papermill $notebook - -f $parameters"
 
-mkdir -p logs/
+mkdir -p $logdir/
 
-if [ "$3" == "--background" ]; then
+if [ "$background" -eq 1 ]; then
     nohup $run_notebook 1> $stdout 2> $stderr &
 else
     $run_notebook 1> $stdout 2> $stderr
