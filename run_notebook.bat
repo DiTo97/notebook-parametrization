63119e799346ea40dcf5a0c19822d54d84ef11f2
Federico Minutoli <40361744+DiTo97@users.noreply.github.com>
2024-02-05 17:58:23 +0100

    update run_notebook.bat

--- run_notebook.bat
+++ run_notebook.bat
@@ -1,19 +1,53 @@
 @echo off
-set notebook=%1
-set config=%2
+REM run_notebook - It runs notebook parametrization in the background
+REM -----------------------------------------------------------------
+set configdir=configs
+set logdir=logs
+set background=0
 
-set parameters=configs\%config%.yaml
+if "%~1"=="" (
+    echo Error: not enough arguments
+    echo Usage: %~nx0 notebook config [--configdir <configdir>] [--logdir <logdir>] [--background]
+    exit /b
+)
 
-set stdout=logs\%config%.out
-set stderr=logs\%config%.err
+set notebook=%~1
+set config=%~2
+
+shift /1
+shift /1
+
+:loop
+if "%~1"=="" goto :continue
+if /i "%~1"=="--configdir" (
+    set configdir=%~2
+    shift /1
+) else if /i "%~1"=="--logdir" (
+    set logdir=%~2
+    shift /1
+) else if /i "%~1"=="--background" (
+    set background=1
+    shift /1
+) else (
+    echo Error: invalid argument
+    echo Usage: %~nx0 notebook config [--configdir <configdir>] [--logdir <logdir>] [--background]
+    exit /b
+)
+goto :loop
+
+:continue
+set parameters=%configdir%\%config%.yaml
+
+set stdout=%logdir%\%config%.out
+set stderr=%logdir%\%config%.err
 
 python preprocessing.py %notebook%
 
 set run_notebook=papermill %notebook% - -f %parameters%
 
-if not exist logs mkdir logs
+if not exist %logdir% mkdir %logdir%
 
-if "%3"=="--background" (
+if "%background%"=="1" (
     start /b cmd /c %run_notebook% 1> %stdout% 2> %stderr%
 ) else (
     %run_notebook% 1> %stdout% 2> %stderr%
